9fc287a5556369126cb9160a259e2fbcb3e3881d
Raul Palomo <raul.palomo.7e7@itb.cat>
2024-04-03 13:07:49 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Text.RegularExpressions;
+namespace Score
+{
+    public static class ScoreProgram
+    {
+        public static void Main()
+        {
+            const string Welcome = "Benvingut a SCORE", MsgName="Escriu el nom de l'explorador: ",MsgMision="Escriu el nom de la misió: ",MsgScore="Escriu la puntuació: ";
+            Console.WriteLine(Welcome);
+            Regex nameRegex = new Regex(@"^[a-zA-Z]+$");
+            Regex misionRegex = new Regex(@"^(Alpha|Beta|Gamma|Delta|Epsilon|Zeta|Eta|Theta|Iota|Kappa|Lambda|Mu|Nu|Xi|Omicron|Pi|Rho|Sigma|Tau|Upsilon|Phi|Chi|Psi|Omega)-\d{3}$");
+            Regex scoreRegex= new Regex(@"^(?:[0-4]?[0-9]?[0-9]|500)$");
+            List<Score> scores = new List<Score>();
+            for (int i = 0; i < 10; i++)
+            {
+                string name="";
+                string mision = "";
+                string score;
+                Console.WriteLine(MsgName);
+                do
+                { 
+                    name=Console.ReadLine();
+                    Console.WriteLine(!nameRegex.IsMatch(name)?"Valor no acceptat":"Valor acceptat");
+                } while (!nameRegex.IsMatch(name));
+                Console.WriteLine(MsgMision);
+                do
+                {
+                    mision = Console.ReadLine();
+                    Console.WriteLine(!misionRegex.IsMatch(mision) ? "Valor no acceptat" : "Valor acceptat");
+
+                } while(!misionRegex.IsMatch(mision));
+                Console.WriteLine(MsgScore);
+                do
+                {
+                    score = Console.ReadLine();
+                    Console.WriteLine(!scoreRegex.IsMatch(score) ? "Valor no acceptat" : "Valor acceptat");
+                } while (!scoreRegex.IsMatch(score));
+                Score newScore=new Score(name,mision,Convert.ToInt32(score));
+                scores.Add(newScore);
+            }
+
+            Score.GenerateUniqueRanking(scores);
+
+
+            
+        }
+    }
+}
---
+++ Score.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Score
+{
+    public class Score
+    {
+        public string Player {  get; set; }
+        public string Mission { get; set; }
+        public int Scoring { get; set; }
+        public Score(string player,string mission, int score) 
+        {
+            Player = player;
+            Mission = mission;
+            Scoring = score;
+        }
+        public static void GenerateUniqueRanking(List <Score>scores)
+        {
+            string name = "";
+            string mision = "";
+            int number = 0;
+            Score last=new Score(name,mision,number);
+            scores.Sort((x, y) => y.Scoring.CompareTo(x.Scoring));
+
+            //Eric, hugo
+            var groupUp = scores.GroupBy(g => g.Player);
+            
+            foreach (var grupo in groupUp)
+            {
+                foreach(Score score in grupo)
+                {
+                    if(last.Player == score.Player&&last.Mission==score.Mission)
+                    {
+                        scores.Remove(score);
+                    }
+                    last= score;
+                }
+            }
+            foreach (var grupo in groupUp)
+            {
+                Console.WriteLine("***************************************************");
+                foreach (Score score in grupo)
+                {
+                    Console.WriteLine($"Jugador: {score.Player}  Misió: {score.Mission}  Puntuació: {score.Scoring}");
+                }
+                Console.WriteLine("***************************************************");
+            }
+        }
+
+    }
+}
